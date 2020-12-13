4fe9f1d71cbe3ee0c4f4d4a21bdabbdc2c0ac3b2
maclyne <margot.clyne@colorado.edu>
2020-12-13 02:03:55 +0000

    re adding fake data test files

--- BRAC_get_permit_data.py
+++ BRAC_get_permit_data.py
@@ -18,6 +18,7 @@ import sys
 import argparse
 from datetime import date
 
+
 def main():
     '''
     I have this here because I couldnt figure out how to only\
@@ -81,12 +82,14 @@ def get_county_names_all(file_name):
 
     # run get_column() to get BRAC permit data
     BRAC_permits_yes = get_column(file_name, permit_column, 'yes',
-                       result_columns=[county_column], date_column=None,
-                       return_dates=False)
+                                  result_columns=[county_column],
+                                  date_column=None,
+                                  return_dates=False)
 
     BRAC_permits_no = get_column(file_name, permit_column, 'no',
-                       result_columns=[county_column], date_column=None,
-                       return_dates=False)
+                                 result_columns=[county_column],
+                                 date_column=None,
+                                 return_dates=False)
 
     countys_yes = BRAC_permits_yes[0]
     countys_no = BRAC_permits_no[0]
@@ -116,29 +119,30 @@ def BRAC_permit_data_with_caserates(BRAC_county_caserates_file,
                                 where the first list is of county names,\
                                 the second list of lists is of dates,\
                                 the third list of lists is of caserates per\
-                                capita (per 100,000 populaiton). The superlists\
+                                capita (per 100,000 populaiton).Thesuperlists\
                                 correspond to the countys, and the \
                                 sublists are the data for each county.
-                                
 
-    BRAC_race_info_file: str 
+
+    BRAC_race_info_file: str
                         name of BRAC permit dataset CSV file
                         example: 'BRAC_races_permits.csv'
 
-                        
+
 
     Returns:
     --------
     out_dataset_file: str   string path name of CSV out file
                         'BRAC_countycases_at_races.csv'
     """
-    # TODO: update doc string b/c BRAC_county_caserates_file is no longer of that format
+    # TODO: update doc str b/c BRAC_county_caserates_file no longer that format
 
     # Import BRAC races data
     state_column = 0
     BRAC_races = get_column(BRAC_race_info_file, state_column, 'Colorado',
-                       result_columns=[1, 2, 3, 4], date_column=None,
-                       return_dates=False)
+                            result_columns=[1, 2, 3, 4],
+                            date_column=None,
+                            return_dates=False)
 
     # make variables from BRAC_races columns easier to follow
     BRAC_races_racenames = BRAC_races[0]
@@ -157,11 +161,12 @@ def BRAC_permit_data_with_caserates(BRAC_county_caserates_file,
     cases_data = [[], [], []]
     for c in range(len(all_countys)):
         cases_data_county = get_column(BRAC_county_caserates_file,
-                                county_column,
-                                all_countys[c],
-                                result_columns=[date_column, caserate_column],
-                                date_column=None,
-                                return_dates=False)
+                                       county_column,
+                                       all_countys[c],
+                                       result_columns=[date_column,
+                                                       caserate_column],
+                                       date_column=None,
+                                       return_dates=False)
         cases_data[0].append(all_countys[c])
         cases_data[1].append(cases_data_county[0])
         cases_data[2].append(cases_data_county[1])
@@ -171,14 +176,13 @@ def BRAC_permit_data_with_caserates(BRAC_county_caserates_file,
     cases_data_dates_lists = cases_data[1]
     cases_data_rates_lists = cases_data[2]
 
-
     # match races to caserates
     caserate_races = []
     for race in range(len(BRAC_races_racenames)):
-            county_ind = cases_data_countys.index(BRAC_races_countys[race])
-            date_ind = cases_data_dates_lists[county_ind].index(BRAC_races_dates[race])
-            caserate_atrace = cases_data_rates_lists[county_ind][date_ind]
-            caserate_races.append(caserate_atrace)
+        county_ind = cases_data_countys.index(BRAC_races_countys[race])
+        date_ind = cases_data_dates_lists[county_ind].index(BRAC_races_dates[race])
+        caserate_atrace = cases_data_rates_lists[county_ind][date_ind]
+        caserate_races.append(caserate_atrace)
 
     # write data to new file that is a copy of old file with added column:
     fin = open(BRAC_race_info_file, 'r')
@@ -186,10 +190,9 @@ def BRAC_permit_data_with_caserates(BRAC_county_caserates_file,
     # skip first header line
     next(fin)
     # parse through file lines
-    for l in fin:
-        out_line_list.append(l)
+    for line in fin:
+        out_line_list.append(line)
     fin.close()
-
 
     out_dataset_file = 'BRAC_countycases_at_races.csv'
     fout = open(out_dataset_file, 'w')
@@ -198,12 +201,13 @@ def BRAC_permit_data_with_caserates(BRAC_county_caserates_file,
                 county caserate per capita 100000 ppl \n")
     # print all lines of previus file but with caserates added as new column
     for line in range(len(BRAC_races_racenames)):
-        new_out_line_list = out_line_list[line].strip()+','+str(caserate_races[line])+ '\n'
+        new_out_line_list = out_line_list[line].strip() + ',' + str(caserate_races[line]) + '\n'
         fout.write(new_out_line_list)
 
     fout.close()
-    
+
     return out_dataset_file
+
 
 if __name__ == '__main__':
     main()
